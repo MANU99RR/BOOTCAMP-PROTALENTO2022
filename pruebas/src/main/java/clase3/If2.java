fbf2384b66ca30efec9eb4ce89f4f31939b8bf6d
Usuario <Usuario@DESKTOP-CUBC9RA>
2022-02-24 22:09:53 -0500

    clase3 if/switch ejercicios

---
+++ pruebas/src/main/java/clase3/If2.java
@@ -0,0 +1,46 @@
+package clase3;
+
+import java.util.Scanner;
+
+public class If2 {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+		
+		//ingrese contraseña y usuario
+		// si contraseña es igual a "1234"  y usuario = admin => "ok" (SE NECESITAN DOS VARIABLES)
+		// de lo contrario "fail"
+		
+		String usuario;
+		String contrasenia;
+		
+		//ASIGNAR
+		usuario="jose";
+		contrasenia="1234";
+		
+		//condiciones
+		if (usuario == "admin" && contrasenia == "1234") {     //TODO LO QUE HAY AQUI ES ERDADERO
+			System.out.println("ok");
+		}else {
+			System.out.println("fail");
+		}
+		
+		//forma anidada
+		if(usuario=="admin") {
+			if(contrasenia=="1234") {
+				System.out.println("ok");
+			}else {
+				System.err.println("fail");
+			}
+			}else {
+				System.err.println("fail");
+				
+		}
+		
+		
+		
+		
+	}
+
+}
